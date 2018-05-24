5af6339fb03ab2cb94e40e6593388e32e697ce8e
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-24 23:31:16 +0000

    Add email confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
-class UserMailer < ApplicationMailer
+class UserMailer < ActionMailer::Base
+  default from: "no-reply@jungle.com"
+  layout 'mailer'
+
+  def order_confirmation(order)
+    @line_items = LineItem.where(order_id: order.id)
+    @order = order
+    mail(to: order.email, subject: "Thanks for your order \##{order.id}!")
+  end
 end
