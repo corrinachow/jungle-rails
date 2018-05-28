b3e348c5b0c68e72d1c35a53c93fa3680747cff8
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-28 17:53:36 +0000

    Add passing base test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:fname) { 'Corrina' }
+  let(:lname) { 'Chow' }
+  let(:email) { 'example@example.com' }
+  let(:password) { 'asdf' }
+  let(:password_confirmation) { 'asdf' }
+
+
+  subject { User.new({
+      fname:  fname,
+      lname: lname,
+      password_confirmation: password_confirmation,
+      password: password,
+      email: email,
+    })}
+
+  context 'new user base' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
 end
