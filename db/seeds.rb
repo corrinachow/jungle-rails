7d97e9f25880569fecfab503de610dbf9b0a81ad
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-28 22:12:11 +0000

    wrapping seed in a transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,151 +7,152 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 puts "Seeding Data ..."
+User.transaction do
+  # Helper functions
+  def open_asset(file_name)
+    File.open(Rails.root.join('db', 'seed_assets', file_name))
+  end
 
-# Helper functions
-def open_asset(file_name)
-  File.open(Rails.root.join('db', 'seed_assets', file_name))
-end
+  # Only run on development (local) instances not on production, etc.
+  unless Rails.env.development?
+    cat1 = Category.find_or_create_by! name: 'Apparel'
+    cat2 = Category.find_or_create_by! name: 'Electronics'
+    cat3 = Category.find_or_create_by! name: 'Furniture'
+  end
 
-# Only run on development (local) instances not on production, etc.
-unless Rails.env.development?
+  # Let's do this ...
+
+  ## USERS
+  puts Faker::Internet.password
+  User.create!({
+    fname: 'Corrina',
+    lname: 'Chow',
+    email: 'example@example.com',
+    password_digest: Faker::Internet.password
+  })
+
+  ## CATEGORIES
+
+  puts "Finding or Creating Categories ..."
+
   cat1 = Category.find_or_create_by! name: 'Apparel'
   cat2 = Category.find_or_create_by! name: 'Electronics'
   cat3 = Category.find_or_create_by! name: 'Furniture'
-end
 
-# Let's do this ...
+  ## PRODUCTS
 
-## USERS
+  puts "Re-creating Products ..."
 
-User.create!({
-  fname: 'Corrina',
-  lname: 'Chow',
-  email: 'example@example.com',
-  password_digest: Faker::Internet.password
-})
+  Product.destroy_all
 
-## CATEGORIES
-
-puts "Finding or Creating Categories ..."
-
-cat1 = Category.find_or_create_by! name: 'Apparel'
-cat2 = Category.find_or_create_by! name: 'Electronics'
-cat3 = Category.find_or_create_by! name: 'Furniture'
-
-## PRODUCTS
-
-puts "Re-creating Products ..."
-
-Product.destroy_all
-
-prod1 = cat1.products.create!({
-  name:  'Men\'s Classy shirt',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel1.jpg'),
-  quantity: 10,
-  price: 64.99
-})
-
-cat1.products.create!({
-  name:  'Women\'s Zebra pants',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel2.jpg'),
-  quantity: 18,
-  price: 124.99
-})
-
-cat1.products.create!({
-  name:  'Hipster Hat',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel3.jpg'),
-  quantity: 4,
-  price: 34.49
-})
-
-cat1.products.create!({
-  name:  'Hipster Socks',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel4.jpg'),
-  quantity: 8,
-  price: 25.00
-})
-
-cat1.products.create!({
-  name:  'Russian Spy Shoes',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel5.jpg'),
-  quantity: 8,
-  price: 1_225.00
-})
-
-cat1.products.create!({
-  name:  'Human Feet Shoes',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel6.jpg'),
-  quantity: 82,
-  price: 224.50
-})
-
-
-cat2.products.create!({
-  name:  'Modern Skateboards',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics1.jpg'),
-  quantity: 40,
-  price: 164.49
-})
-
-cat2.products.create!({
-  name:  'Hotdog Slicer',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics2.jpg'),
-  quantity: 3,
-  price: 26.00
-})
-
-cat2.products.create!({
-  name:  'World\'s Largest Smartwatch',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics3.jpg'),
-  quantity: 32,
-  price: 2_026.29
-})
-
-cat3.products.create!({
-  name:  'Optimal Sleeping Bed',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture1.jpg'),
-  quantity: 320,
-  price: 3_052.00
-})
-
-cat3.products.create!({
-  name:  'Electric Chair',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture2.jpg'),
-  quantity: 0,
-  price: 987.65
-})
-
-cat3.products.create!({
-  name:  'Red Bookshelf',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture3.jpg'),
-  quantity: 0,
-  price: 2_483.75
-})
-
-## REVIEWS
-
-Product.all.each do |product|
-  5.times do
-    product.reviews.create!({
-    user_id: 1,
-    description: Faker::TwinPeaks.quote,
-    rating: Faker::Number.between(1, 5) ,
+  prod1 = cat1.products.create!({
+    name:  'Men\'s Classy shirt',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel1.jpg'),
+    quantity: 10,
+    price: 64.99
   })
-  end
-end
 
-puts "DONE!"
+  cat1.products.create!({
+    name:  'Women\'s Zebra pants',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel2.jpg'),
+    quantity: 18,
+    price: 124.99
+  })
+
+  cat1.products.create!({
+    name:  'Hipster Hat',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel3.jpg'),
+    quantity: 4,
+    price: 34.49
+  })
+
+  cat1.products.create!({
+    name:  'Hipster Socks',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel4.jpg'),
+    quantity: 8,
+    price: 25.00
+  })
+
+  cat1.products.create!({
+    name:  'Russian Spy Shoes',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel5.jpg'),
+    quantity: 8,
+    price: 1_225.00
+  })
+
+  cat1.products.create!({
+    name:  'Human Feet Shoes',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel6.jpg'),
+    quantity: 82,
+    price: 224.50
+  })
+
+
+  cat2.products.create!({
+    name:  'Modern Skateboards',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('electronics1.jpg'),
+    quantity: 40,
+    price: 164.49
+  })
+
+  cat2.products.create!({
+    name:  'Hotdog Slicer',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('electronics2.jpg'),
+    quantity: 3,
+    price: 26.00
+  })
+
+  cat2.products.create!({
+    name:  'World\'s Largest Smartwatch',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('electronics3.jpg'),
+    quantity: 32,
+    price: 2_026.29
+  })
+
+  cat3.products.create!({
+    name:  'Optimal Sleeping Bed',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('furniture1.jpg'),
+    quantity: 320,
+    price: 3_052.00
+  })
+
+  cat3.products.create!({
+    name:  'Electric Chair',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('furniture2.jpg'),
+    quantity: 0,
+    price: 987.65
+  })
+
+  cat3.products.create!({
+    name:  'Red Bookshelf',
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('furniture3.jpg'),
+    quantity: 0,
+    price: 2_483.75
+  })
+
+  ## REVIEWS
+
+  Product.all.each do |product|
+    5.times do
+      product.reviews.create!({
+      user_id: 1,
+      description: Faker::TwinPeaks.quote,
+      rating: Faker::Number.between(1, 5) ,
+    })
+    end
+  end
+
+  puts "DONE!"
+end
