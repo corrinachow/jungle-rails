b7ebe84a2694221b9ef0be3cacef36174032458f
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-28 18:08:40 +0000

    Add password validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,15 @@ RSpec.describe User, type: :model do
     end
   end
 
+  context 'password' do
+
+    let(:password_confirmation) { 'asdfasdf' }
+
+    it 'passwords should do not match' do
+      expect(subject).to_not be_valid
+      puts subject.errors.messages
+      expect(subject.errors.messages[:password_confirmation]).to include("doesn't match Password")
+    end
+  end
+
 end
