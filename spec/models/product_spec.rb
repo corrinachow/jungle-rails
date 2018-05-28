d28c2d28f9342c9cde05e710e294ef6b5d36d82d
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-28 17:40:56 +0000

    Add product validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Product, type: :model do
 
   let(:name) { 'Coffee' }
   let(:description) { 'asdf' }
-  let(:price) { '1111' }
+  let(:price) { 1111 }
   let(:qty) { 1 }
   let(:image) { 'https://i.ytimg.com/vi/kYnx7nPw-xo/maxresdefault.jpg' }
 
@@ -32,20 +32,30 @@ RSpec.describe Product, type: :model do
     it 'does not have a category' do
       expect(subject.category_id).to be_nil
     end
-
   end
 
 
-  # context 'new product no name' do
+  context 'new product no name' do
+    let(:name) { nil }
+    it 'does not have name' do
+      expect(subject).to be_invalid
+      expect(subject.errors.messages[:name]).to include("can't be blank")
+    end
+  end
 
-  #   let(:name) { nil }
+  context 'new product no quantity' do
+    let(:qty) { nil }
+    it 'does not have quantity' do
+      expect(subject).to be_invalid
+      expect(subject.errors.messages[:quantity]).to include("can't be blank")
+    end
+  end
 
-  #   it 'does not have name' do
-  #     expect(subject).to be_invalid
-  #   end
-
-  # end
-
-
+  context 'new product no price' do
+    let(:price) { nil }
+    it 'does not have price' do
+      expect(subject.price).not_to eq(nil)
+    end
+  end
 
 end
