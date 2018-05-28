54337053147400ee8872a78476e1ebd91a6dd38d
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-28 17:18:57 +0000

    Add nnil category test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
 
-  cat1 = Category.find_or_create_by! name: 'Food'
+  let(:category) { Category.new name: 'Food'}
 
   let(:name) { 'Coffee' }
   let(:description) { 'asdf' }
@@ -11,12 +11,13 @@ RSpec.describe Product, type: :model do
   let(:image) { 'https://i.ytimg.com/vi/kYnx7nPw-xo/maxresdefault.jpg' }
 
 
-  subject {cat1.products.new({
+  subject { Product.new({
       name:  name,
       description: description,
       image: image,
       quantity: qty,
-      price: price
+      price: price,
+      category: category
     })}
 
   context 'new product base' do
@@ -25,15 +26,14 @@ RSpec.describe Product, type: :model do
     end
   end
 
-  # context 'new product no category' do
+  context 'new product no category' do
+    let(:category) { nil }
 
-  #   subject.category_id = nil
+    it 'does not have a category' do
+      expect(subject.category_id).to be_nil
+    end
 
-  #   it 'does not have a category' do
-  #     expect(subject.category_id).to be_nil
-  #   end
-
-  # end
+  end
 
 
   # context 'new product no name' do
